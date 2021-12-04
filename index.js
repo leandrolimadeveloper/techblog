7719d2d12b4e0e12de69f3ccc0701276a5a8a3e4
leandrolimadeveloper <leandroslimasr@gmail.com>
2021-12-04 12:05:47 -0300

    Filtering articles by category

--- index.js
+++ index.js
@@ -62,6 +62,28 @@ app.get('/:slug', (req, res) => {
     })
 })
 
+app.get('/category/:slug', (req, res) => {
+    let slug = req.params.slug
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then(category => {
+        if(category != undefined) {
+            
+            Category.findAll().then(categories => {
+                res.render('index', {articles: category.articles, categories: categories})
+            })
+
+        } else {
+            res.redirect('/')
+        }
+    }).catch(err => {
+        res.redirect('/')
+    })
+})
+
 app.listen(8080, () => {
     console.log('O servidor está rodando.')
 })
