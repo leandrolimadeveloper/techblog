bf3a4c9223c9fc60bbdd9ae4637d2d655599fb20
leandrolimadeveloper <leandroslimasr@gmail.com>
2021-12-09 15:54:46 -0300

    Created pagionation Created pagination for the page, inserting the number page manually. It is still necessary to create the buttons to navigate among the pages

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -89,4 +89,38 @@ router.post('/articles/update', (req, res) => {
     })
 })
 
+router.get('/articles/page/:num', (req, res) => {
+    let page = req.params.num
+    let limit = 4
+    let offset = 0
+
+    if(isNaN(page) || page == 1) {
+        offset = 0
+    } else {
+        offset = parseInt(page) * limit
+    }
+
+    Article.findAndCountAll({
+        limit: limit,
+        offset: offset
+    }).then(articles => {
+        let next 
+        if(offset + limit >= articles.count) {
+            next = false
+        } else {
+            next = true
+        }
+
+        let result = {
+            next: next,
+            articles: articles
+        }
+
+        Category.findAll().then(categories => {
+            res.render('admin/articles/page', {result: result, categories: categories})
+        })
+
+    })
+})
+
 module.exports = router
